a38fdad6f450c009e0823397e29e4b4df7182a2a
james-knott-iw <james.knott@integration.works>
2024-05-03 10:12:52 +1200

    remove unnecessary functions

--- order-read.cs
+++ order-read.cs
@@ -36,42 +36,7 @@ namespace IntegrationWorks.Function
             string? connectionString = System.Environment.GetEnvironmentVariable("ORDER_READ_QUEUE_KEY");
             if (connectionString != null)
             {
-                string queueName = "order-read-queue";
 
-                // the client that owns the connection and can be used to create senders and receivers
-                ServiceBusClient client;
-
-                // the sender used to publish messages to the queue
-                ServiceBusSender sender;
-
-                client = new ServiceBusClient(connectionString);
-                sender = client.CreateSender(queueName);
-
-                // create a batch
-                using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
-
-                Message message = new Message { Id = id };
-
-                if (!messageBatch.TryAddMessage(new ServiceBusMessage(JsonSerializer.Serialize(message))))
-                {
-
-                    throw new Exception($"Exception {message.Id} has occured.");
-                }
-
-                try
-                {
-                    await sender.SendMessagesAsync(messageBatch);
-                    _logger.LogInformation($"Sent message: {message} to the queue");
-                }
-                finally
-                {
-                    // Calling DisposeAsync on client types is required to ensure that network
-                    // resources and other unmanaged objects are properly cleaned up.
-                    await sender.DisposeAsync();
-                    await client.DisposeAsync();
-                }
-
-                _logger.LogInformation("C# HTTP trigger function processed a request.");
                 return new OkObjectResult("Welcome to Azure Functions!");
             }
             return new OkObjectResult("Connection String null");
